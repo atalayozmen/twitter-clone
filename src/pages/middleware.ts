4c5880372ce2717121bd3e3823db493af82d806c
Atalay Özmen <Atalay>
2023-05-18 20:52:26 +0200

    Added auth

---
+++ src/pages/middleware.ts
@@ -0,0 +1,9 @@
+//this code runs after every request
+
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware();
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
